3e4c9d473449e6456dfd807a3f80df82cbf5a52c
Tom Sebastian <tomsebastiantom@gmail.com>
2022-02-28 20:48:53 -0800

    Final Refactor

--- src/app/iframepage/iframepage.component.ts
+++ src/app/iframepage/iframepage.component.ts
@@ -1,74 +1,74 @@
-import { Component, OnInit,ViewChild,HostListener,AfterViewInit } from '@angular/core';
-import {NgxChessBoardView} from 'ngx-chess-board';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  HostListener,
+  AfterViewInit,
+} from '@angular/core';
+import { NgxChessBoardView } from 'ngx-chess-board';
 
 @Component({
   selector: 'app-iframepage',
   templateUrl: './iframepage.component.html',
-  styleUrls: ['./iframepage.component.css']
+  styleUrls: ['./iframepage.component.css'],
 })
-
 export class IframepageComponent implements AfterViewInit, OnInit {
-
-  constructor() { }
-restoreFromLocal(){
-  console.log("restore from local")
+  constructor() {}
+  restoreFromLocal() {
     const state = localStorage.getItem('chessdata');
-    if(state){
-      console.log(state);
-    this.setFen(state);
+    if (state) {
+      this.setFen(state);
     }
-    // if(state){
-    //   this.postMessage("iframe1",{"fen":state});
-    //   this.postMessage("iframe2",{"fen":state});
-    // }
   }
-@HostListener('window:message', ['$event'])
+  @HostListener('window:message', ['$event'])
   handleMessageEvent(event: MessageEvent) {
-       if(!event.data.type){
-               this.setFen(event.data.fen);
-             if(event.data.reset === true){
-                this.reset();
-                 }         
-           }
+    if (!event.data.type) {
+      this.setFen(event.data.fen);
+      if (event.data.reset === true) {
+        this.reset();
+      }
     }
+  }
 
   handlechange(event: any): void {
-   
     const fen = this.getFen();
     const history = this.getHistory();
-    window.parent.postMessage({"fen":fen,"move":history[history.length-1].move,"checkmate":history[history.length-1].check,"source":window.frameElement?.id}, "*");
-  
+    window.parent.postMessage(
+      {
+        fen: fen,
+        move: history[history.length - 1].move,
+        checkmate: history[history.length - 1].check,
+        source: window.frameElement?.id,
+      },
+      '*'
+    );
   }
-  @ViewChild("board",{static: false}) board: NgxChessBoardView | undefined;
-  reset():void{
-      this.board?.reset();        
-    }  
-  getFen():string|undefined{
-       let fen:string |undefined="";
-       fen = this.board?.getFEN();
-       return fen;
-  }  
-  setFen(fen: string):void{
-     this.board?.setFEN(fen);    
+  @ViewChild('board', { static: false }) board: NgxChessBoardView | undefined;
+  reset(): void {
+    this.board?.reset();
   }
-  
-  getHistory():any|undefined{
-     let history:any |undefined="";
-     history = this.board?.getMoveHistory();
-     return history;
-   }
-   
-  setMove(move:string):void{
-       this.board?.move(move);
-     }
+  getFen(): string | undefined {
+    let fen: string | undefined = '';
+    fen = this.board?.getFEN();
+    return fen;
+  }
+  setFen(fen: string): void {
+    this.board?.setFEN(fen);
+  }
 
-  ngOnInit(): void {
-    
+  getHistory(): any | undefined {
+    let history: any | undefined = '';
+    history = this.board?.getMoveHistory();
+    return history;
   }
+
+  setMove(move: string): void {
+    this.board?.move(move);
+  }
+
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
-    this.restoreFromLocal()
+    this.restoreFromLocal();
   }
-  
-
 }
